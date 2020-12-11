d6c824a7ad26aaa2644241d3e0e99655382675d6
Ganesh Jagadeesan <ganesh.jcs@gmail.com>
2020-12-11 17:14:52 -0500

    fixing decoder

--- nmt/networks/decoder.py
+++ nmt/networks/decoder.py
@@ -8,12 +8,15 @@ from typing import Tuple
 
 class Decoder(nn.Module):
     def __init__(self, input_size: int,
-                 hidden_size: int, 
+                 hidden_size: int,
                  bias: bool = True,
                  dropout_prob: float = 0.3):
         super(Decoder, self).__init__()
         self.hidden_size = hidden_size
-        self.attention = Attention()
+        self.attention = Attention(
+            in_features=hidden_size*2,
+            out_features=hidden_size
+        )
         self.decoder = nn.LSTMCell(
             input_size=input_size,
             hidden_size=hidden_size,
@@ -25,38 +28,26 @@ class Decoder(nn.Module):
             bias=False
         )
         self.dropout = nn.Dropout(p=dropout_prob)
-        
-    def forward(self, output: torch.Tensor,
+
+    def forward(self, x: torch.Tensor,
                 enc_hidden: torch.Tensor,
-                enc_projection: torch.Tensor,
                 dec_init_state: Tuple[torch.Tensor, torch.Tensor],
-                enc_masks: torch.Tensor = None,
-                device: torch.device = 'cpu') -> torch.Tensor:
-        
+                o_prev: torch.Tensor,
+                enc_masks: torch.Tensor = None) -> torch.Tensor:
+
         dec_state = dec_init_state
-        batch_size, sent_length, _ = enc_hidden.size()
 
-        o_prev = torch.zeros(batch_size, self.hidden_size, device=device)
-        combined_outputs = []
+        Ybar_t = torch.cat([x.squeeze(dim=0), o_prev], dim=1)
 
-        for Y_t in torch.split(output, 1, dim=0):
-            Ybar_t = torch.cat([Y_t.squeeze(dim=0), o_prev], dim=1)
+        dec_state = self.decoder(Ybar_t, dec_state)
+        dec_hidden, dec_cell = dec_state
 
-            dec_state = self.decoder(Ybar_t, dec_state)
-            dec_hidden, dec_cell = dec_state
+        attention_scores, context_vector = self.attention(
+            enc_hidden, dec_hidden, enc_masks
+        )
 
-            attention = self.attention(enc_projection, dec_hidden, enc_masks)
-            attention = attention.unsqueeze(dim=1)
+        U_t = torch.cat([context_vector, dec_hidden], dim=1)
+        V_t = self.combined_projection(U_t)
+        output = self.dropout(V_t.tanh())
 
-            a_t = attention.bmm(enc_hidden)
-            a_t = a_t.squeeze(dim=1)
-            
-            U_t = torch.cat([a_t, dec_hidden], dim=1)
-            V_t = self.combined_projection(U_t)
-            o_t = self.dropout(V_t.tanh())
-
-            combined_outputs.append(o_t)
-            o_prev = o_t
-
-        combined_outputs = torch.stack(combined_outputs, dim=0)
-        return combined_outputs
+        return output, dec_state, attention_scores
